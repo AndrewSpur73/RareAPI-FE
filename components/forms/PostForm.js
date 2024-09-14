020e61f426d08198207467adb8488320be494eb4
Ross Morgan <ross.morgan933@gmail.com>
2024-09-14 13:56:47 -0500

    form is populating

--- components/forms/PostForm.js
+++ components/forms/PostForm.js
@@ -1,51 +1,69 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import PropTypes from 'prop-types';
 import Form from 'react-bootstrap/Form';
 import { Button } from 'react-bootstrap';
 import { useAuth } from '../../utils/context/authContext';
 import { createPost, updatePost } from '../../api/postData';
-// import { getAllTags } from '../../api/tagData';
+import { getAllTags } from '../../api/tagData';
 
 const initialState = {
   title: '',
   content: '',
   category: '',
   imageUrl: '',
-  tags: [],
+  tagId: [],
 };
 
 export default function PostForm({ obj }) {
   const [formInput, setFormInput] = useState(initialState);
-  const [tags] = useState([]);
+  const [tags, setTags] = useState([]);
   const router = useRouter();
   const { user } = useAuth();
-  // useEffect(() => {
-  //   getAllTags().then(setTags);
-  //   if (obj.id) setFormInput(obj);
-  // }, [obj, user]);
+  useEffect(() => {
+    getAllTags().then(setTags);
+    if (obj.id) setFormInput({ ...obj, tagIds: obj.postTags.map((tag) => tag.tag.id) });
+  }, [obj, user]);
+
   const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormInput((prevState) => ({
-      ...prevState,
-      [name]: value,
-    }));
+    const {
+      name, type, checked, value,
+    } = e.target;
+    if (type === 'checkbox') {
+      const currentTagIds = [...formInput.tagIds];
+      const tagId = parseInt(e.target.value, 10); // Assuming value attribute holds tag id
+      if (checked) {
+        currentTagIds.push(tagId);
+      } else {
+        const index = currentTagIds.indexOf(tagId);
+        currentTagIds.splice(index, 1);
+      }
+      setFormInput((prevState) => ({
+        ...prevState,
+        tagIds: currentTagIds,
+      }));
+    } else {
+      // Handle other input types (text, url, etc.)
+      setFormInput((prevState) => ({
+        ...prevState,
+        [name]: value,
+      }));
+    }
   };
-  const handleTagChange = (event) => {
-    const selectedOptions = Array.from(event.target.selectedOptions, (option) => option.value);
-    setFormInput((prevState) => ({
-      ...prevState,
-      tags: selectedOptions, // Update the state with an array of selected tag IDs
-    }));
-  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
     if (obj.id) {
-      // Update Post then View Post
+      console.warn(formInput);
       updatePost(formInput).then(() => router.push('/feed'));
     } else {
-      const payload = { ...formInput, userId: user.id };
-      createPost(payload).then(() => router.push('/feed'));
+      const payload = { ...formInput, tagIds: formInput.tagId, uid: user.uid };
+      createPost(payload).then((title) => {
+        const patchPayload = { id: title };
+        updatePost(patchPayload).then(() => {
+          router.push('/feed');
+        });
+      });
     }
   };
   return (
@@ -55,34 +73,56 @@ export default function PostForm({ obj }) {
           <Form.Label>{obj.id ? 'Update' : 'Create'} Post</Form.Label>
           {/* TITLE INPUT  */}
           <Form.Group controlId="formTitle" className="mb-3">
-            <Form.Control type="text" placeholder="Enter a title" name="title" value={formInput.title} onChange={handleChange} className="input rounded-none" required />
+            <Form.Control
+              type="text"
+              placeholder="Enter a title"
+              name="title"
+              value={formInput.title}
+              onChange={handleChange}
+              className="input rounded-none"
+              required
+            />
           </Form.Group>
           {/* POST CONTENT TEXTAREA  */}
           <Form.Group controlId="formContent" className="mb-3">
-            <Form.Control as="textarea" placeholder="Content" style={{ height: '100px' }} name="content" value={formInput.content} onChange={handleChange} className="input rounded-none" required />
+            <Form.Control
+              as="textarea"
+              placeholder="Content"
+              style={{ height: '100px' }}
+              name="content"
+              value={formInput.content}
+              onChange={handleChange}
+              className="input rounded-none"
+              required
+            />
           </Form.Group>
+
           {/* POST IMAGE URL  */}
           <Form.Group controlId="formBasicImage" className="mb-3">
-            <Form.Control type="url" name="image" placeholder="Enter an image URL" value={formInput.imageUrl} onChange={handleChange} className="input rounded-none" />
+            <Form.Control
+              type="text"
+              name="imageUrl"
+              placeholder="Enter an image URL"
+              value={formInput.imageUrl || ''}
+              onChange={handleChange}
+              className="input rounded-none"
+            />
           </Form.Group>
-          {/* TAG SELECT  */}
-          <Form.Group controlId="formSelect" className="mb-3">
-            <Form.Select
-              aria-label="Tags"
-              name="tags"
-              onChange={handleTagChange}
-              className="mb-3 rounded-sm select"
-              multiple
-              value={obj.tag} // This should be an array of selected tag IDs
-              required
-            >
-              {tags.map((tag) => (
-                <option key={tag.id} value={tag.id}>
-                  {tag.label}
-                </option>
-              ))}
-            </Form.Select>
-          </Form.Group>
+          <div>
+            <b>Tags: </b>
+            {tags.map((tag) => (
+              <label key={tag.id}>
+                <input
+                  type="checkbox"
+                  value={tag.id}
+                  onChange={handleChange}
+                  checked={formInput.tagIds.includes(tag.id)}
+                />
+                {tag.name}
+              </label>
+            ))}
+          </div>
+
           {/* SUBMIT BUTTON  */}
           <Button type="submit" className="form-button">
             {obj.id ? 'Update' : 'Create'} Post
@@ -96,11 +136,16 @@ PostForm.propTypes = {
   obj: PropTypes.shape({
     id: PropTypes.number,
     userId: PropTypes.number,
-    tagId: PropTypes.number,
+    tagIds: PropTypes.arrayOf(PropTypes.number),
     title: PropTypes.string,
     content: PropTypes.string,
     imageUrl: PropTypes.string,
     tag: PropTypes.string,
+    postTags: PropTypes.arrayOf(PropTypes.shape({ // Define postTags as an array of objects
+      tag: PropTypes.shape({
+        id: PropTypes.number.isRequired, // Ensure the tag has an id
+      }),
+    })),
   }),
 };
 PostForm.defaultProps = {
--- pages/post/edit/[id].js
+++ pages/post/edit/[id].js
@@ -1,18 +1,18 @@
-// import React, { useEffect, useState } from 'react';
-// import { useRouter } from 'next/router';
-// import { getSinglePost } from '../../../api/postData';
-// import PostForm from '../../../components/forms/PostForm';
+import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import { getSinglePost } from '../../../api/postData';
+import PostForm from '../../../components/forms/PostForm';
 
-// export default function EditPost() {
-//   const [editItem, setEditItem] = useState({});
-//   const router = useRouter();
-//   const { id } = router.query;
+export default function EditPost() {
+  const [editItem, setEditItem] = useState({});
+  const router = useRouter();
+  const { id } = router.query;
 
-//   // make a call to the API to get the member data
-//   useEffect(() => {
-//     getSinglePost(id).then(setEditItem);
-//   }, [id]);
+  // make a call to the API to get the member data
+  useEffect(() => {
+    getSinglePost(id).then(setEditItem);
+  }, [id]);
 
-//   // pass object to form
-//   return (<PostForm obj={editItem} />);
-// }
+  // pass object to form
+  return (<PostForm obj={editItem} />);
+}
